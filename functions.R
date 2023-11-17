4ae202958718865f36b3112efbe956a784d6a0c1
George Alehandro Saad <georges_saad1998@hotmail.com>
2023-11-17 10:08:11 +0100

    uploading docker changes

--- functions.R
+++ functions.R
@@ -11,9 +11,8 @@ convert_to_BD_FacsDiva <- function(inputFile, outputFile) {
   indices_max <- grep("max", names(fcs_data@description))
   indices_PnR <- grep("P\\d+R$", names(fcs_data@description))
   
-  # Compute the maximum value among these parameters
-  max_value_max <- max(unlist(fcs_data@description[indices_max]))
-  max_value_PnR <- max(unlist(fcs_data@description[indices_PnR]))
+  max_value_max <- 262144
+  max_value_PnR <- 262144
   
   # Set these parameters to their respective maximum value
   for (index in indices_max){
@@ -23,6 +22,35 @@ convert_to_BD_FacsDiva <- function(inputFile, outputFile) {
     fcs_data@description[[index]] <- max_value_PnR
   }
   
+  # Extract the expression matrix
+  expr_matrix <- exprs(fcs_data)
+  
+  # Get parameter data, including max ranges
+  params <- parameters(fcs_data)
+  
+  # Extract maxRanges using varLabels
+  max_ranges <- as.numeric(params$`maxRange`)
+  
+  # Identify columns without "-W" or "time" in their names for scaling
+  valid_cols <- !grepl("-W|time", params$name, ignore.case = TRUE)
+  
+  # Extract maxRanges for valid columns
+  valid_max_ranges <- max_ranges[valid_cols]
+  
+  # Compute the scaling factors by dividing the valid maxRange by 262143
+  scaling_factors <- valid_max_ranges / 262143
+  
+  # Scale the expression matrix using the valid columns
+  expr_matrix[, valid_cols] <- sweep(expr_matrix[, valid_cols], 2, scaling_factors, `/`)
+  
+  # Replace the original expression matrix with the scaled matrix
+  exprs(fcs_data) <- expr_matrix
+  
+  # Update the maxRange to 262143 for ALL parameters
+  params$`maxRange`[] <- 262143
+  params$`range`[] <- 262143
+  parameters(fcs_data) <- params
   # Write the result to a new FCS file
+  
   write.FCS(fcs_data, outputFile)
 }
